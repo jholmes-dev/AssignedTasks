ad3c2edf22039bdf85b62e1f68d6589bd16564e5
Jeffrey Holmes <coffeman500@gmail.com>
2024-09-24 23:05:42 -0500

    Set up confirmation dialog

--- AssTasks.Client/src/app/components/modals/manage-tasks-modal/manage-tasks-modal.component.ts
+++ AssTasks.Client/src/app/components/modals/manage-tasks-modal/manage-tasks-modal.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatIcon } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 
 import { TaskParent } from '../../../models/task-parent';
 import { AssTaskParentService } from '../../../services/ass-task-parent.service';
+import { ConfirmationModalComponent } from '../confirmation-modal/confirmation-modal.component';
 
 @Component({
   selector: 'app-manage-tasks-modal',
@@ -18,17 +20,19 @@ import { AssTaskParentService } from '../../../services/ass-task-parent.service'
     MatButtonModule,
     MatInputModule,
     ReactiveFormsModule,
-    MatTableModule
+    MatTableModule,
+    MatIcon
   ],
   templateUrl: './manage-tasks-modal.component.html',
   styleUrl: './manage-tasks-modal.component.scss'
 })
 export class ManageTasksModalComponent {
+  readonly confirmModal = inject(MatDialog);
   public assTaskParents: TaskParent[] = [];
-  public displayedColumns: string[] = [ "createdAt", "title", "active" ];
+  public displayedColumns: string[] = [ "createdAt", "title", "active", "controls" ];
 
   constructor(
-    private dialogReg: MatDialogRef<ManageTasksModalComponent>,
+    private dialogRef: MatDialogRef<ManageTasksModalComponent>,
     private assTaskParentService: AssTaskParentService
   ) {}
 
@@ -37,6 +41,15 @@ export class ManageTasksModalComponent {
     this.assTaskParentService.getTaskParents().subscribe({
       next: (taskParents: TaskParent[]) => {
         this.assTaskParents = taskParents;
+      }
+    });
+  }
+
+  showConfirmation() {
+    this.confirmModal.open(ConfirmationModalComponent, {
+      data: {
+        title: "Confirm Deletion",
+        content: "Are you sure you'd like to delete this record?"
       }
     });
   }
