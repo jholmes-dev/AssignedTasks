6f32af5a6d25259e0f8a62aa9aa885f388efc92c
Jeffrey Holmes <jeffreysholmess@gmail.com>
2025-04-05 15:28:12 -0500

    Adding routing for create

--- AssTasks.Client/src/app/app.routes.ts
+++ AssTasks.Client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CreateTaskComponent } from "./components/create-task/create-task.component";
+import { TaskGridComponent } from "./components/task-grid/task-grid.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: TaskGridComponent },
+    { path: 'create-task', component: CreateTaskComponent },
+];
